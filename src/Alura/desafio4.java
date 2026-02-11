f6a3cdfdd7a0e3e19fb9a4d295d3811c300a76df
Anderson Siqueira Junior <andersonsiqueirajunior898@gmail.com>
2026-02-11 08:27:59 -0300

    Mais exercicios

---
+++ src/Alura/desafio4.java
@@ -0,0 +1,66 @@
+package Alura;
+
+import java.util.Scanner;
+
+public class desafio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /* 
+        Desafio 1
+        System.out.println("DIGITE UM NUMERO");
+        int numA = sc.nextInt();
+        if (numA > 0) {
+            System.out.println("Numero positivo");
+            
+        } else {
+            System.out.println("Numero negativo");
+        }
+        */
+
+        /* 
+        Atividade 2
+        System.out.println("PASSE DOIS NUMEROS INTEIROS ABAIXO");
+        int numB = sc.nextInt();
+        int numC = sc.nextInt();
+
+        if (numB == numC) {
+            System.out.println("Numero iguais");
+        } else {
+            System.out.println("Numeros diferentes");
+            if (numB > numC) {
+                System.out.println("Primeiro numero maior");
+            }else{
+                System.out.println("Segundo numero maior");
+            }
+        }
+
+        */
+            
+        
+
+        sc.close();
+
+        
+    
+
+
+/*
+Crie um programa que solicite ao usuário digitar um número. Se o número for positivo, exiba "Número positivo", 
+caso contrário, exiba "Número negativo".
+
+Peça ao usuário para inserir dois números inteiros. Compare os números e imprima uma 
+mensagem indicando se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
+
+Crie um menu que oferece duas opções ao usuário: "1. Calcular área do quadrado" e "2. Calcular 
+área do círculo". Solicite a escolha do usuário e realize o cálculo da área com base na opção selecionada.
+
+Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.
+
+Crie um programa que solicite ao usuário a entrada de um número inteiro. Verifique se o número é par
+ ou ímpar e exiba uma mensagem correspondente.
+ 
+Crie um programa que solicite ao usuário um número e calcule o fatorial desse número. 
+*/
+
+}}
