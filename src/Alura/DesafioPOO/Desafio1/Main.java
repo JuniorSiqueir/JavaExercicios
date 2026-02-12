5c52ffd44f65106ad184a6e685681c4f4aff52e5
Anderson Siqueira Junior <andersonsiqueirajunior898@gmail.com>
2026-02-12 13:01:45 -0300

    Mais coisa

--- src/Alura/DesafioPOO/Desafio1/Main.java
+++ src/Alura/DesafioPOO/Desafio1/Main.java
@@ -23,5 +23,27 @@ public class Main {
         m1.avaliacao(3);
         System.out.println(m1.numeroDeAvaliacoes);
         System.out.println(m1.calculaMedia());
+
+        carro carro1 = new carro();
+        carro1.cor = "Azul";
+        carro1.modelo = "Rapido";
+        carro1.anoModelo = 2000;
+        carro1.mostraFicha();
     }
+
+
+
+    /*
+    Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
+
+    Crie uma classe Calculadora com um método que recebe um número como parâmetro e retorna o dobro desse número.
+
+    Crie uma classe Musica com atributos titulo, artista, anoLancamento, avaliacao e numAvaliacoes, e métodos para 
+    exibir a ficha técnica, avaliar a música e calcular a média de avaliações.
+
+    Crie uma classe Carro com atributos modelo, ano, cor e métodos para exibir a ficha técnica e calcular a idade do carro.
+
+    Crie uma classe Aluno com atributos nome, idade, e um método para exibir informações. Crie uma instância da classe Aluno, 
+    atribua valores aos seus atributos e utilize o método para exibir as informações.
+    */
 }
---
+++ src/Alura/DesafioPOO/Desafio1/carro.java
@@ -0,0 +1,16 @@
+package Alura.DesafioPOO.Desafio1;
+
+public class carro {
+    String modelo;
+    int anoModelo;
+    String cor;
+
+    void mostraFicha(){
+        System.out.println("Cor: " + cor);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + anoModelo);
+    }
+
+    
+
+}
